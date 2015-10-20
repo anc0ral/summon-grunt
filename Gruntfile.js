4bd50d4c0268499487751f10a5ae44a0c1513c00
Andrew P <anc0ral@users.noreply.github.com>
2015-10-21 00:45:02 +1100

    Added object to contain variables & added calls to the variables inside the data function

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,34 @@ module.exports = function(grunt) {
 	// Automatically tracks time for each Grunt process
 	require('time-grunt')(grunt);
 
+	var object = {
+		path : require('path'),
+		current : process.cwd(),
+		grunt : 'grunt',
+		get configuration() {
+			return this.grunt + '/configuration'
+		},
+		get override() {
+			return this.grunt + '/Overridefolders.yaml'
+		}
+	}
+
 	require('load-grunt-config')(grunt, {
 
 		/* Makes use of the JIT Grunt plug-in’s default path for external files to load the custom tasks
 		   'overridePath' is being used to keep the configuration files seperate from the build tasks */
-		overridePath : require('path').join(process.cwd(), 'grunt/configuration'),
+		configPath: [
+			object.path.join(object.current, object.grunt),
+			object.path.join(object.current, object.configuration)
+		],
 
 		data: {
 			folders: (function() {
 
-				if (grunt.file.isFile('grunt/Overridefolders.yaml')) {
-					return grunt.file.readYAML('grunt/Overridefolders.yaml')
+				if (grunt.file.isFile(object.override)) {
+					return grunt.file.readYAML(object.override)
 				} else {
-					return grunt.file.readYAML('grunt/folders.yaml')
+					return grunt.file.readYAML(object.grunt + '/folders.yaml')
 				}
 
 			}())
